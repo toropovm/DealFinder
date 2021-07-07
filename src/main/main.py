a938cc7626dd8e4c2092f5554696603f2cfe1690
Mykola Toropov <toropovm@gmail.com>
2021-07-07 00:47:37 -0400

    DF-1: Obtaining price from Newegg product page

--- src/main/main.py
+++ src/main/main.py
@@ -1,9 +1,22 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+	
 
 
 def main():
-    driver = webdriver.Chrome("C:\\Users\\Haysus\\Desktop\\Python Project\\chromedriver.exe")
-    driver.get("google.com")
+    driver = webdriver.Edge(r"C:\Users\Haysus\Desktop\Python Project\msedgedriver.exe")
+    driver.get("https://www.newegg.com/amd-ryzen-9-3900x/p/N82E16819113103?quicklink=false")
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+
+    product_price = soup.find(string="\"Buy New\"").find_parent(class_="product-pane").find(class_="price-current").find("strong").string
+    time.sleep(2)
+    print("Price is this: " + product_price)
+    
 
 if __name__ == '__main__':
     main()
+
+
+
